b01e8f06249242e1834a06bd749230af59354abb
JoacoAlmada <almadajoaco26@gmail.com>
2025-07-29 17:16:15 -0300

    Proyecto inicial

---
+++ Back/Sistema de Reservas para Gimnasio/src/main/java/com/gimnasio/reservassistema/SistemaDeReservasParaGimnasioApplication.java
@@ -0,0 +1,13 @@
+package com.gimnasio.reservassistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeReservasParaGimnasioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeReservasParaGimnasioApplication.class, args);
+	}
+
+}
